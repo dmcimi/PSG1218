04878fff7b9a5315eaccd5cada7761344ae2967a
小和 <61643603+dmcimi@users.noreply.github.com>
2021-02-07 03:27:01 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,6 +8,16 @@
 #1. Modify default IP
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#=================================================
+# 清除默认主题
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
+#=================================================
+# 清除旧版argon主题并拉取最新版
+pushd package/lean
+rm -rf luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
+popd
+#=================================================
 
 
 
